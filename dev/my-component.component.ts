884b88c3dc007031d39d2b3ade2d29b2ce7d7bc9
jaguso <jagutzo@gmail.com>
2018-12-03 19:12:28 -0600

    changed font color of name

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -4,7 +4,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-component',
     template: `
-        Hi, I'm {{name}} and this is my very first 
+        Hi, I'm <span [style.color]="'red'">{{name}}</span> and this is my very first 
     Angular 2 component bitches. <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome</span>
         <br>
         <br>
